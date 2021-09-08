9394ce76954ba97233358d3ce9337c661d39bb2c
Kevin <kevin.chang@revature.net>
2021-09-08 14:39:42 -0400

    MVP achieved

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -2,9 +2,32 @@ import {Col} from "react-bootstrap";
 
 const Detail = (props: any) => {
     return(
+        props.currentUser ?
+
         <>
-            {props.currentUser ? props.currentUser.email : "Empty Details"}
+
+            <div id="user-details-div" style={{position: "fixed"}}>
+                <h1>
+                    {props.currentUser.fullName}
+                </h1>
+                <p>
+                    Username: {props.currentUser.username} <br/>
+                    Password: {props.currentUser.password} <br/>
+                    Age: {props.currentUser.age} <br/>
+                    Email: {props.currentUser.email} <br/> <br/>
+                    {props.currentUser.description}
+                </p>
+
+            </div>
         </>
+            :
+            <>
+                <div>
+                    <h1>
+                        No User Selected
+                    </h1>
+                </div>
+            </>
     );
 };
 
